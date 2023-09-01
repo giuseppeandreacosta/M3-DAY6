e56ca31822dbd835998bc5059c18665a95384c54
giuseppeandreacosta <giuseppeandreac@gmail.com>
2023-09-02 00:09:10 +0200

    js upgrade,css upgrade,html upgrade.

--- javascript/index.js
+++ javascript/index.js
@@ -1,16 +1,24 @@
-// Dichiaro la funzione che convalida il checkbox al button della pagina iniziale.
-
-
-window.onload = function() {
+/* funzione loader caricamento pagina */
+function loader(){            
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+/* setto l'intervallo di tempo del caricamento quando scompare */
+function fadeOut(){             
+  setInterval(loader, 6400);
+}
+/* quando si carica scompare */
+window.onload = fadeOut();
     
     function enable() {
         let check = document.querySelector("#check-boxed");
         let buttonNextTo = document.querySelector("#buttonNextTo");
         if (check.checked) {
-          buttonNextTo.removeAttribute("disabled");
+          buttonNextTo.removeAttribute("enabled");
         } else {
-          buttonNextTo.disabled = "true";
+          buttonNextTo.disabled = "false";
         }
       }
     enable();
-  };
+
+    
+  
